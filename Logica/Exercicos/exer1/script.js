eb00af067b1523dd2c5c78dbe575432b3c7b26a5
Cleiton Paes <cleiton03122000@gmail.com>
2022-06-25 12:13:45 -0300

    Fazendo mudança na na arvore de conteudo. Adicionando mais exercicios. Realiazando Ex01 a Ex08 indo fazer o Ex09

--- Logica/Exercicos/exer1/script.js
+++
@@ -1,9 +0,0 @@
-// Ex01
-let media = 0
-let nota1 = parseFloat(prompt('Nota 1: '))
-let nota2 = parseFloat(prompt('Nota 2: '))
-let nota3 = parseFloat(prompt('Nota 3: '))
-let nota4 = parseFloat(prompt('Nota 4: '))
-media = nota1 + nota2 + nota3 + nota4
-media /= 4
-document.write('Media das notas: ' + media)
