de4802be0dd8fe8ac7cbfba430ca1bc456ed7dd0
Cleiton Paes <cleiton03122000@gmail.com>
2022-07-16 08:32:08 -0300

    Arrumando Ex2

--- Logica/Aula2/Exercicio/Ex02/script.js
+++ Logica/Aula2/Exercicio/Ex02/script.js
@@ -4,6 +4,8 @@ while(true)
     n1 = parseFloat(prompt("Digite um numero: "))
     if (n1 >= 0 && n1 <= 10)
     {
+        document.write('Nota: ' + n1 + '<br>')
         break
     }
+    document.write("Numero invalido")
 }
